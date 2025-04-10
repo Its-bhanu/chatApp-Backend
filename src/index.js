ec7c10b2276eb7993ad04defdd3e6b1002092ffd
Its-bhanu <bhanu.22it498@rtu.ac.in>
2025-04-10 17:50:49 +0530

    new

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chat-me-rosy-eta.vercel.app",
+    origin: "*",
     credentials: true,
   })
 );
--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://chat-me-rosy-eta.vercel.app"],
+    origin: ["https://chat-app-111.vercel.app"],
   },
 });
 
